600f64d2f8ebd750bbfd9e0688044b151aaeb615
Moritz Baten <moritz.baten@cancom.de>
2021-01-18 23:49:30 +0100

    improve main and start overview

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from process_data import process_data_and_create_plots
+from test_prepare import data
+from test_plot import plots
 
-process_data_and_create_plots(country='de')
+country='de'
+# variation A
+# data=data(country)
+# data.load(data.attr.url,country)
 
-process_data_and_create_plots(country='lv')
+data=data(country)
+data.load(country)
+
+plots_obj=plots(data.attr)
+plots_obj.plot_smoothened(data.smooth,country)
+plots_obj.plot_threshold(data.simulated,country)
+print('done')
+
